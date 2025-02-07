f2512edbce73f2015fd5458d8566b59dae720a8f
Chrispin <67915167+ChrispinP@users.noreply.github.com>
2025-02-07 12:20:05 -0600

    Create knice.js

---
+++ knice.js
@@ -0,0 +1,5 @@
+const searchResultDiv = document.querySelector('.flex.flex-col.gap-4');
+        const descriptionElement = document.createElement('p');
+        descriptionElement.textContent = 'This is a description';
+        descriptionElement.className = 'description';
+        searchResultDiv.appendChild(descriptionElement);
